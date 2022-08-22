8e70896da0efaa4fd4c6b8b9656bcbd17d883049
Karpukhin Egor <karpukhin.egor@gmail.com>
2022-08-22 15:57:43 +0300

    Custom temp preference ended

--- Clowy/ProfileView/ProfileSubViews/TempPreferenceView.swift
+++ Clowy/ProfileView/ProfileSubViews/TempPreferenceView.swift
@@ -8,8 +8,10 @@
 import SwiftUI
 
 struct TempPreferenceView: View {
-    @State private var speed = 0.0
+    @State private var speed = 0.5
     @State private var isEditing = false
+    
+    @State var sliderColor = UIColor(Color(hex: "#FFDDA9"))
     
     
     var body: some View {
@@ -24,54 +26,37 @@ struct TempPreferenceView: View {
                         .font(.custom("Montserrat-Medium", size: 16))
                     Spacer()
                 }
-                RoundedRectangle(cornerRadius: 3)
-                    .foregroundColor(Color(hex: "#FFDDA9"))
-                    .frame(height: 4)
-                
-                Slider(
-                    value: $speed,
-                    in: -10...10,
-                    step: 0.5
-                ) {
-                    Text("Speed")
-                } minimumValueLabel: {
-                    Text("0")
-                } maximumValueLabel: {
-                    Text("100")
-                } onEditingChanged: { editing in
-                    isEditing = editing
-                }
                 Text("\(speed)")
                     .foregroundColor(isEditing ? .red : .blue)
                 
-                
                 SwiftUISlider(
-                        thumbColor: UIColor(
-                            if value > 0 {
-                                Color(hex: "#FFA929").opacity(modul)
-                            } else if value == 0 {
-                                Color(hex: "#D9D9D9")
-                            } else {
-                                Color(hex: "#6391EB").opacity(modul)
-                            }
-                        ),
-                        minTrackColor: UIColor(getColor(value: speed)),
-                        maxTrackColor: UIColor(getColor(value: speed)),
-                        value: $speed
-                      ).padding(.horizontal)
+                    thumbColor: .white,
+                    minTrackColor: UIColor(Color(hex: "##BFD6EF")),
+                    maxTrackColor: UIColor(Color(hex: "##BFD6EF")),
+                    value: $speed
+                )
                 
-                HStack {
-                    Text("Easier")
-                        .foregroundColor(Color(hex: "#646C75"))
-                        .font(.custom("Montserrat-Regular", size: 12))
+                
+                HStack(alignment: .center) {
+                    HStack {
+                        Text("Easier")
+                            .foregroundColor(Color(hex: "#646C75"))
+                            .font(.custom("Montserrat-Regular", size: 12))
+                        Spacer()
+                    }
+                    .frame(width: 73)
                     Spacer()
                     Rectangle()
                         .frame(width: 1, height: 16)
                         .foregroundColor(Color(hex: "#DBE4EF"))
                     Spacer()
-                    Text("Warmer")
-                        .foregroundColor(Color(hex: "#646C75"))
-                        .font(.custom("Montserrat-Regular", size: 12))
+                    HStack {
+                        Spacer()
+                        Text("Warmer")
+                            .foregroundColor(Color(hex: "#646C75"))
+                            .font(.custom("Montserrat-Regular", size: 12))
+                    }
+                    .frame(width: 73)
                 }
             }
             .padding(16)
